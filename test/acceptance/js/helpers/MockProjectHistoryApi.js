647ca310b2430f67b3f203511b5b552530b5062a
Tim Alby <timothee.alby@gmail.com>
2020-01-13 18:16:37 +0100

    prettier: convert test/acceptance decaffeinated files to Prettier format

--- test/acceptance/js/helpers/MockProjectHistoryApi.js
+++ test/acceptance/js/helpers/MockProjectHistoryApi.js
@@ -10,31 +10,35 @@
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-let MockProjectHistoryApi;
-const express = require("express");
-const app = express();
+let MockProjectHistoryApi
+const express = require('express')
+const app = express()
 
-module.exports = (MockProjectHistoryApi = {
-	flushProject(doc_id, callback) {
-		if (callback == null) { callback = function(error) {}; }
-		return callback();
-	},
+module.exports = MockProjectHistoryApi = {
+  flushProject(doc_id, callback) {
+    if (callback == null) {
+      callback = function(error) {}
+    }
+    return callback()
+  },
 
-	run() {
-		app.post("/project/:project_id/flush", (req, res, next) => {
-			return this.flushProject(req.params.project_id, function(error) {
-				if (error != null) {
-					return res.send(500);
-				} else {
-					return res.send(204);
-				}
-			});
-		});
+  run() {
+    app.post('/project/:project_id/flush', (req, res, next) => {
+      return this.flushProject(req.params.project_id, function(error) {
+        if (error != null) {
+          return res.send(500)
+        } else {
+          return res.send(204)
+        }
+      })
+    })
 
-		return app.listen(3054, function(error) {
-			if (error != null) { throw error; }
-		});
-	}
-});
+    return app.listen(3054, function(error) {
+      if (error != null) {
+        throw error
+      }
+    })
+  }
+}
 
-MockProjectHistoryApi.run();
+MockProjectHistoryApi.run()
